34cb830fc5c7b3edfea9d52b9106f33f3f7457e2
Yew Brown <brown.voss32@gmail.com>
2025-11-26 16:14:16 +0000

    reverted redirectLogin url in books.js

--- routes/books.js
+++ routes/books.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 const redirectLogin = (req, res, next) => {
     if (!req.session.userId ) {
-        res.redirect('users/login') // redirect to the login page
+        res.redirect('/users/login') // redirect to the login page
     } else {
         next (); // move to the next middleware function
     } 
